707fd663b9b7b6b0790a46cd82ff7fa9d94c77b6
ShahabARS <shahab.ars.7@gmail.com>
2025-08-20 18:59:15 +0330

    feat: Add skills and education sections to the resume with progress indicators. next step: adding style (In Progress) to those skills that haven't yet reached the desired level/l

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const skillsProgress = document.querySelectorAll(".skills-item__progress");
+skillsProgress.forEach((item) => {
+  let skillLevel = +item.getAttribute("skill-level");
+  item.style.setProperty(
+    "--skill-level",
+    item.getAttribute("skill-level") + "%"
+  );
+  console.log(typeof skillLevel);
+});
