584424a8fb1bee1945eca85dab6f14529522e226
Shahab <shahab.ars.7@gmail.com>
2025-08-27 17:34:54 +0330

    add a Example Resume Landing Page, create full responsive design for Education and Skills section

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const skillsProgress = document.querySelectorAll(".skills-item__progress");
-skillsProgress.forEach((item) => {
-  let skillLevel = +item.getAttribute("skill-level");
-  item.style.setProperty(
-    "--skill-level",
-    item.getAttribute("skill-level") + "%"
-  );
-  console.log(typeof skillLevel);
+const progressBars = document.querySelectorAll(".skills-item__progress");
+
+progressBars.forEach((item) => {
+  const level = parseInt(item.getAttribute("skill-level"), 10);
+  item.style.setProperty("--skill-level", level + "%");
+  if (level < 30) {
+    item.style.setProperty("--progress-color", "#be0000");
+  } else {
+    item.style.setProperty("--progress-color", "#cbe601");
+  }
 });
